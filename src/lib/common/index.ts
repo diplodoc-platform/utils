f105a20020e5015a2117b336a8de31ace0a49f81
Sergey Zolotonos <gold-serg@yandex-team.ru>
2026-03-25 18:18:05 +0300

    feat: Universal generateID method DOCSTOOLS-5561

--- src/lib/common/index.ts
+++ src/lib/common/index.ts
@@ -4,6 +4,8 @@ export type {
     ScriptStore,
 } from './extension-load-queue';
 
+export type {IDGenerator} from './id-generator';
+
 export {AttrsParser} from './attrs';
 
 export {parseMdAttrs} from './parse-md-attrs';
@@ -11,3 +13,5 @@ export {parseMdAttrs} from './parse-md-attrs';
 export {createLoadQueue, getQueueStore, getScriptStore} from './extension-load-queue';
 
 export {isBrowser} from './browser';
+
+export {createIDGenerator} from './id-generator';
